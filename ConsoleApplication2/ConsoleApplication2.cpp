941de67e5478581af8301b3ce46f2c907fd36441
nekoromant <igorbog842@gmail.com>
2023-10-05 17:12:49 +0700

    Initial commit

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,29 @@
+﻿// C Вариант 1
+// Дано три целых числа. Вывести количество отричальных и чётных чисел
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int n[4];
+    int a=0;
+    int b=0;
+    for (int i = 1; i <= 3; i++) {
+        cout << "Введите " << i << "-е число: ";
+        cin >> n[i];
+    }
+    for (int i = 1; i <= 3; i++) { 
+        if (n[i] <= 0) a++;
+        if (n[i] % 2 == 0) b++;
+    }
+
+    cout << "Количество отрицательных чисел: " << a << endl;
+    cout << "Количество четных чисел: " << b << endl;
+
+    system("pause");
+    return 0;
+}
+
+
