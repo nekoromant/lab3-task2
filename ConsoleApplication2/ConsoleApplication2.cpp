70cd0e159e381d448c9757917dee1ce40d8f9162
nekoromant <igorbog842@gmail.com>
2023-10-12 10:24:27 +0700

    n2

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -2,28 +2,47 @@
 // Дано три целых числа. Вывести количество отричальных и чётных чисел
 
 #include <iostream>
-using namespace std;
 
-int main()
-{
-    setlocale(LC_ALL, "rus");
-    int n[4];
-    int a=0;
-    int b=0;
-    for (int i = 1; i <= 3; i++) {
-        cout << "Введите " << i << "-е число: ";
-        cin >> n[i];
+    int main() {
+        setlocale(LC_ALL, "rus");
+        using namespace std;
+        int num1, num2, num3;
+        int evenNum = 0;
+        int negativуNum = 0;
+
+        // Ввод трех натуральных чисел
+        cout << "Введите три натуральных числа: "<< endl;
+        cout << "Введите первое число: ";
+        cin >> num1;
+        cout << "Введите второе число: ";
+        cin >> num2;
+        cout << "Введите третье число: ";
+        cin >> num3;
+        // Проверка каждого числа на чёрное и отрицательное
+        if (num1 % 2 == 0) {
+            evenNum++;
+        }
+        if (num2 % 2 == 0) {
+            evenNum++;
+        }
+        if (num3 % 2 == 0) {
+            evenNum++;
+        }
+        if (num1 < 0) {
+            negativуNum++;
+        }
+        if (num2 < 0) {
+            negativуNum++;
+        }
+        if (num3 < 0) {
+            negativуNum++;
+        }
+
+        // Вывод результата
+        cout << "Количество чёрных чисел: " << evenNum << endl;
+        cout << "Количество отрицательных чисел: " << negativуNum << endl;
+
+        return 0;
     }
-    for (int i = 1; i <= 3; i++) { 
-        if (n[i] <= 0) a++;
-        if (n[i] % 2 == 0) b++;
-    }
-
-    cout << "Количество отрицательных чисел: " << a << endl;
-    cout << "Количество четных чисел: " << b << endl;
-
-    system("pause");
-    return 0;
-}
 
 
